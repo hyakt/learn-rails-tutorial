f06323faf03f076738fd34cbc78861a625b0fd0e
hyakt <hyakt0@gmail.com>
2019-11-19 18:37:10 +0900

    4.1.2 https://railstutorial.jp/chapters/rails_flavored_ruby?version=5.1#sec-custom_helpers

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,16 +1,10 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
-  test "should get root" do
-    get root_url
-    assert_response :success
-    assert_select "title", "Home | Ruby on Rails Tutorial Sample App"
-  end
-
   test "should get home" do
     get static_pages_home_url
     assert_response :success
-    assert_select "title", "Home | Ruby on Rails Tutorial Sample App"
+    assert_select "title", "Ruby on Rails Tutorial Sample App"
   end
 
   test "should get help" do
@@ -19,16 +13,15 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
     assert_select "title", "Help | Ruby on Rails Tutorial Sample App"
   end
 
-  test "shold get about " do
+  test "shold get about" do
     get static_pages_about_url
     assert_response :success
     assert_select "title", "About | Ruby on Rails Tutorial Sample App"
   end
 
-  test "shold get contact " do
+  test "shold get contact" do
     get static_pages_contact_url
     assert_response :success
     assert_select "title", "Contact | Ruby on Rails Tutorial Sample App"
   end
-
 end
