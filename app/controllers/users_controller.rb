da5631867b0f7a8c6d555038c5e8cda95903c1af
hyakt <hyakt0@gmail.com>
2019-12-18 00:16:08 +0900

    7.3.3

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,13 +11,15 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       # handle save
+    else
+      render 'new'
     end
-    render 'new'
   end
 
   private
 
     def user_params
-      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+      params.require(:user).permit(:name, :email, :password,
+                                   :password_confirmation)
     end
 end
