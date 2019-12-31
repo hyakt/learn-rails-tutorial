abee61eb5b30c9cf7bbacbbb5954667adf8e4be7
hyakt <hyakt0@gmail.com>
2019-12-31 18:03:20 +0900

    fix: 9.1.1

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,17 +5,17 @@ class User < ApplicationRecord
   validates :name, presence: true, length: {maximum: 50}
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
   validates :email, presence: true,
-              length: {maximum: 255},
-              format: { with: VALID_EMAIL_REGEX },
-              uniqueness: { case_sensitive: false }
+            length: {maximum: 255},
+            format: { with: VALID_EMAIL_REGEX },
+            uniqueness: { case_sensitive: false }
   has_secure_password
   validates :password, presence: true, length: { minimum: 6 }
 
- class << self
+  class << self
     # 渡された文字列のハッシュ値を返す
     def digest(string)
       cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
-                                                    BCrypt::Engine.cost
+               BCrypt::Engine.cost
       BCrypt::Password.create(string, cost: cost)
     end
 
@@ -23,12 +23,16 @@ class User < ApplicationRecord
     def new_token
       SecureRandom.urlsafe_base64
     end
- end
+  end
 
   def remember
     self.remember_token = User.new_token
-    update_attribute(:rememember_digest, User.digest(remember_token))
+    update_attribute(:remember_digest, User.digest(remember_token))
   end
 
-
 end
+
+
+
+
+
